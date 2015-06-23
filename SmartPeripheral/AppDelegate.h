0428baefd4961ef75f1fb1598e3d0572abcdeddc
hemantgore <gore.hemant@yahoo.co.in>
2015-06-23 23:36:15 +0530

    Initial Commit

---
+++ SmartPeripheral/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmartPeripheral
+//
+//  Created by Sandeep on 23/06/15.
+//  Copyright (c) 2015 H. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SmartPeripheral/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmartPeripheral
+//
+//  Created by Sandeep on 23/06/15.
+//  Copyright (c) 2015 H. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
